a5b8065c221f102a6e7e30a7e1c18f3ebb1b21a1
Mateusz Jura <matthew98jura@gmail.com>
2019-01-16 12:22:37 +0100

    first step to repair navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import './Navbar.css';
 import {loginRegLink} from './LoginRegLink'
 import SvgIcon from '@material-ui/core/SvgIcon';
-
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 
 
@@ -19,56 +20,51 @@ class Navbar extends Component {
 
 
 
-        render() {
+    render() {
 
-            function HomeIcon(props) {
-                return (
-                    <SvgIcon {...props}>
-                        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                    </SvgIcon>
-                );
-            }
-            const userLink = (
+        function HomeIcon(props) {
+            return (
+                <SvgIcon {...props}>
+                    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+                </SvgIcon>
+            );
+        }
+        const userLink = (
 
-                <AppBar position="static" className="AppBarContainer">
-                    <ul className="AppBarContainer">
-                        <div className="itemNav">
-                            <Link to="/" >
-                                <HomeIcon color="action" className="itemNav"/>
-                            </Link>
-                        </div>
-                        <div className="itemNav">
-                            <Link to="/profile" className="nav-link">
-                                User
-                            </Link>
-                        </div>
-                        <div>
-                            <a href="#" onClick={this.logOut.bind(this)} className="nav-link">
-                                Logout
-                            </a>
-                        </div>
-                        <div>
-                            <Link to="/createEvent" className="itemNav">
-                                Create event
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/allEvents" className="itemNav">
-                                all events
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/userEvents" className="itemNav">
-                                Your Events
-                            </Link>
-                        </div>
-                    </ul>
-                </AppBar>
+            <AppBar position="static" className="AppBarContainer">
+                <Grid container direction="row" justify="flex-start" alignItems="center" spacing={16}>
+                    <Grid item >
+                        <Link to="/" >
+                            <HomeIcon color="action" className="itemNav" />
+                        </Link>
+                    </Grid>
+                    <Grid item >
+                        <Link to="/profile" className="nav-link">
+                            <Button variant="outlined" color="default"> User </Button>
+                        </Link>
+                    </Grid>
+                    <Grid item >
+                        <a href="#" onClick={this.logOut.bind(this)} className="nav-link">
+                            <Button variant="outlined" color="default">Logout</Button>
+                        </a>
+                    </Grid>
+                    <Grid item >
+                        <Link to="/createEvent" className="itemNav">
+                            <Button variant="outlined" color="default">Create event</Button>
+                        </Link>
+                    </Grid>
+                    <Grid item >
+                        <Link to="/allEvents" className="itemNav">
+                            <Button variant="outlined" color="default">all events</Button>
+                        </Link>
+                    </Grid>
+                </Grid>
+            </AppBar>
 
-            )
+        )
         return (
             <nav className="AppBarContainer">
-                    {localStorage.usertoken ? userLink : loginRegLink}
+                {localStorage.usertoken ? userLink : loginRegLink}
             </nav>
 
         )
